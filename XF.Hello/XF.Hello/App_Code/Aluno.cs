badc92274571b2634591f09bdebbb172e5bb3c59
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-23 22:36:09 -0300

    Add project files.

---
+++ XF.Hello/XF.Hello/App_Code/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Hello.NewFolder
+{
+    class Aluno
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ XF.Hello/XF.Hello/DataBindingPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Hello.NewFolder;
+
+namespace XF.Hello
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DataBindingPage : ContentPage
+	{
+		public DataBindingPage ()
+		{
+            Aluno aluno = new Aluno()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "João Silva",
+                Email = "joao@fiap.com"
+            };
+
+            BindingContext = aluno;
+
+			InitializeComponent ();
+		}
+	}
+}
